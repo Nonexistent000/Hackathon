74187d79fdff0f504f4d9c583e3335cea8e3f088
Nonexistent000 <65271125+Nonexistent000@users.noreply.github.com>
2020-10-24 20:58:08 -0400

    Add files via upload

---
+++ About.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class About extends JFrame {
+    private JButton back = new JButton("Back");
+    public About() {
+
+        Container contentPane;
+        contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        add(back);
+        back.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
+        back.setBounds(0, 800, 200, 200);
+
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new Menu();
+
+            }
+
+
+        });
+
+        setSize(1000,800);
+        setResizable(false);
+        setLocationRelativeTo(null);//centres window
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("About");
+    }
+}
